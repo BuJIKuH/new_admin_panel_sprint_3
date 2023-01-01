23f15a481c679fb3f9449786e0d2fe47ec05d80b
BuJIKuH <klassniy.jew@gmail.com>
2023-01-01 22:04:15 +0300

    [INFO] подредактировал state.py

--- etl/state.py
+++ etl/state.py
@@ -32,17 +32,10 @@ class JsonFileStorage(BaseStorage):
 
     def retrieve_state(self) -> dict:
         try:
-
             with open(self.file_path) as file:
-                # читаем json
                 return json.load(file)
-
         except (FileNotFoundError, json.JSONDecodeError):
-
-            # если возникло исклчюение
             with open(self.file_path, 'w', encoding='utf8') as file:
-
-                # создаем json с дефолтным значением self.default_state
                 json.dump(self.default_state, file)
 
             return self.retrieve_state()
@@ -51,12 +44,8 @@ class JsonFileStorage(BaseStorage):
         '''
         метод сохранения состояния в json файле
         '''
-
-        # получаем состояние из json
         data = self.retrieve_state()
-        # обновляем состояние на новое
         data.update(state)
-        # записываем его в json
         with open(self.file_path, 'w', encoding='utf8') as file:
             json.dump(data, file)
 
